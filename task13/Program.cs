adafc910e2cdd87c28200b574a7c24f7850d2221
kostya <kotsazonov@gmail.com>
2023-06-14 15:10:25 +0300

    home work

---
+++ task13/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+    
+Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int thirdDigit = GetThirdDigit(number);
+
+        if (thirdDigit != -1)
+        {
+            Console.WriteLine($"Третья цифра числа: {thirdDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    
+
+    static int GetThirdDigit(int number)
+    {
+        int digitCount = (int)Math.Floor(Math.Log10(number) + 1);
+
+        if (digitCount >= 3)
+        {
+            int thirdDigit = (number / (int)Math.Pow(10, digitCount - 3)) % 10;
+            return thirdDigit;
+        }
+
+        return -1;
+    }
+
+
+    
---
+++ task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//  и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+ Console.WriteLine("Введите цифру, обозначающую день недели (от 1 до 7):");
+        int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+        bool isWeekend = IsWeekend(dayOfWeek);
+
+        if (isWeekend)
+        {
+            Console.WriteLine("Да, это выходной день.");
+        }
+        else
+        {
+            Console.WriteLine("Нет, это не выходной день.");
+        }
+
+        Console.ReadLine();
+    
+
+    static bool IsWeekend(int dayOfWeek)
+    {
+        return dayOfWeek == 6 || dayOfWeek == 7;
+    }
